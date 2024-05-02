ab70f6314a26263d58ec3d94a4613f8829828a8d
Pujangithub12 <168169955+Pujangithub12@users.noreply.github.com>
2024-05-02 12:08:49 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import Header from './components/Header'
+import Balance from './components/Balance';
+import IncomeExpences from './components/IncomeExpences';
+import TransactionLists from './components/TransactionLists';
+import AddTransaction from './AddTransaction';
 import './App.css';
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpences />
+        <TransactionLists />
+        <AddTransaction />
+      </div>
+
+    </GlobalProvider>
   );
 }
 
